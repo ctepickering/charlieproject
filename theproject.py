e8bf85d413ae25610b1837de17f938a19d3ddb9d
ctepickering <pickeringcte@dulwich.org.uk>
2024-09-24 21:22:48 +0100

    making first map

--- theproject.py
+++ theproject.py
@@ -1,5 +1,6 @@
 import pygame
-
+from pygame.locals import *
+pygame.init()
 # Screen setup
 screen_x =650
 screen_y = 650
@@ -19,12 +20,61 @@ YELLOW = ((255,255,0))
 
 background = pygame.image.load('Background.png')
 
-world_data= []
 
+tile_size = 25
 class World():
     def __init__(self,data) -> None:
-        
-        block_img = pygame.image.load('blockimg.png')
+        self.tile_list = []
+        block_img = pygame.image.load('block.png')
+        row_pos = 0
+        for row in data :
+            column_pos = 0
+            for tile in row :
+                if tile == 1 : # the tile is a block #
+                    img = pygame.transform.scale(block_img, (tile_size, tile_size)) #scale block image to size of block
+                    img_rect = img.get_rect()
+                    img_rect.x = column_pos * tile_size
+                    img_rect.y = row_pos * tile_size
+                    tile = (img,img_rect)
+                    self.tile_list.append(tile)
+                column_pos = column_pos +1
+            row_pos =row_pos +1
+    def draw(self) :
+        for tile in self.tile_list:
+            screen.blit(tile[0],tile[1])
+        # next tile
+    #end procedure
+
+level1_map= [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1],
+[1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+[1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+
+
+level1= World(level1_map)
+
 
 done= False
 clock = pygame.time.Clock()
@@ -38,8 +88,8 @@ while not done:
     # --- Game logic should go here
 
     screen.blit(background, (0,0))
-
-    pygame.display.flip()
+    level1.draw()
+    pygame.display.update()
     
     #clock.tick(60)
 #endwhile
