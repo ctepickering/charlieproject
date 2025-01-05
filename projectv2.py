a4e467e626bf3d40a7aebf614387d84e2d0e3b36
ctepickering <pickeringcte@dulwich.org.uk>
2025-01-05 12:51:58 +0000

    Update projectv2.py

--- projectv2.py
+++ projectv2.py
@@ -1,7 +1,13 @@
+from typing import Any
 import pygame
 from pygame.locals import *
+import math
 
 pygame.init()
+
+clock = pygame.time.Clock()
+
+level = 1
 
 # Screen setup #
 
@@ -26,11 +32,16 @@ YELLOW = ((255,255,0))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,x,y):
+        super().__init__()
         self.image = pygame.Surface([20,25])
         self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y= y
+        self.startx = x
+        self.starty = y
+        self.width=self.image.get_width()
+        self.height=self.image.get_height()
         self.vel_y = 0
         self.isJumped = False
     #end func
@@ -40,6 +51,7 @@ class Player(pygame.sprite.Sprite):
         # draw player # 
 
         screen.blit(self.image,self.rect)
+        pygame.draw.rect(screen, WHITE, self.rect, 1)
 
         # player movement #
 
@@ -48,17 +60,14 @@ class Player(pygame.sprite.Sprite):
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            change_x -= 2.5
+            change_x -= 3
         #end if
         if keys[pygame.K_RIGHT]:
-            change_x += 2.5
+            change_x += 3
         # end if  
-        if keys[pygame.K_UP]and self.isJumped == False:
+        if keys[pygame.K_UP] and self.isJumped == False:
             self.vel_y = -10
             self.isJumped=True
-        #end if
-        if keys[pygame.K_UP]:
-            self.isJumped = False
         #end if
 
         # calculate gravity #
@@ -69,6 +78,31 @@ class Player(pygame.sprite.Sprite):
         #end if
 
         change_y +=self.vel_y
+
+        # check player collision #
+
+        if level == 1 :
+            world = level1
+
+        for tile in world.tile_list :
+            # check for collision in x #
+            if tile[1].colliderect(self.rect.x +change_x, self.rect.y, self.width, self.height) :
+                change_x = 0
+            # check for collision in y #
+            if tile[1].colliderect(self.rect.x, self.rect.y + change_y, self.width, self.height) :
+                # check if bellow the ground #
+                if self.vel_y < 0 :
+                    change_y = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                elif self.vel_y >= 0 :
+                    change_y = tile[1].top - self.rect.bottom
+                    self.isJumped=False
+
+        # collision with enemies # 
+        if pygame.sprite.spritecollide(self, spike_group, False):
+            self.rect.x = self.startx
+            self.rect.y = self.starty
+
         # update player coordinates #
 
         self.rect.x += change_x
@@ -78,6 +112,31 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom = screen_y 
             change_y = 0
         #end if
+
+class Enemy_type1(pygame.sprite.Sprite):
+    def __init__(self,startx,starty,direction):
+        super().__init__()
+        self.width = 20
+        self.height = 20
+        self.image = pygame.Surface([20,20])
+        self.image.fill(ORANGE)
+        self.rect = self.image.get_rect()
+        self.rect.x = startx
+        self.rect.y = starty
+        self.anchor_pos = startx
+        self.step = 0
+        self.direction = direction
+    #end constructor
+    def update(self):
+            screen.blit(self.image,self.rect)
+            pygame.draw.rect(screen, WHITE, self.rect, 1)
+            if self.direction == 'p' :
+                self.step += 0.02
+            else :
+                self.step -= 0.02
+            self.rect.x =  self. anchor_pos +200* math.sin(self.step)
+    #end function
+#end class
 
 
 tile_size = 25
@@ -96,14 +155,21 @@ class World():
                     img_rect.y = row_pos * tile_size
                     tile = (img,img_rect)
                     self.tile_list.append(tile)
+                if tile == 2 : # check if tile is enemy p direction #
+                    spike = Enemy_type1(column_pos * tile_size,row_pos * tile_size, 'p')
+                    spike_group.add(spike)
+                if tile == 3 : # check if tile is enemy n direction #
+                    spike = Enemy_type1(column_pos * tile_size,row_pos * tile_size, 'n')
+                    spike_group.add(spike)
                 column_pos = column_pos +1
             row_pos =row_pos +1
+
     #end func
     def draw(self) :
         for tile in self.tile_list:
             screen.blit(tile[0],tile[1])
+            pygame.draw.rect(screen, WHITE, tile[1], 1)
     #end func
-
 
 
 level1_map= [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -113,12 +179,12 @@ level1_map= [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,0,0,0,0,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1],
 [1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1,1],
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1],
-[1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1],
+[1,1,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,1,1,0,0,1,1],
 [1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
 [1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
 [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,2,3,0,0,0,0,0,0,0,0,0,0,0,1],
 [1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1],
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
@@ -135,8 +201,11 @@ level1_map= [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 
 # Objects #
 
-player1 = Player(30, 605)
-Level1= World(level1_map)
+player1 = Player(30, 600)
+
+spike_group = pygame.sprite.Group()
+
+level1= World(level1_map)
 
 
 # Main Program Loop #
@@ -152,8 +221,10 @@ while not done:
     
     # Game logic #
 
-    #screen.fill(BLACK)
-    Level1.draw()
+    clock.tick(60)
+    screen.fill(BLACK)
+    level1.draw()
+    spike_group.update()
     player1.update()
 
     pygame.display.update()
