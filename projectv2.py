01da79fb25af0f8be7369ac1f934cdbddaaa0759
ctepickering <pickeringcte@dulwich.org.uk>
2025-04-02 13:05:44 +0100

    screens

--- projectv2.py
+++ projectv2.py
@@ -3,6 +3,8 @@ import pygame
 from pygame.locals import *
 import math
 import random
+import textwrap
+
 
 pygame.init()
 
@@ -14,14 +16,20 @@ screen_x = 650
 screen_y = 650
 size = (screen_x, screen_y)
 
+# Load the custom font #
+
+font = pygame.font.Font("PressStart2P.ttf", 36)
+font2 = pygame.font.Font("PressStart2P.ttf", 60)
+font3 = pygame.font.Font("PressStart2P.ttf", 18)
+
 b_image = pygame.image.load('Background.png')
 background = pygame.transform.scale(b_image, (screen_x, screen_y))
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Tiny Robot")
 
-level = 1
-
+level = -1
+maxlevel = 5
 # Colours #
 
 WHITE = ((255,255,255))
@@ -33,6 +41,38 @@ ORANGE = ((255,100,10))
 YELLOW = ((255,255,0))
 
 # Classes #
+
+class Button:
+    def __init__(self, x, y, width, height, text, font, colour, text_colour):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.font = font
+        self.colour = colour
+        self.text_colour = text_colour
+
+    def draw(self, screen):
+
+        #draw 
+        pygame.draw.rect(screen, self.colour, self.rect, border_radius=10)
+        
+        # Render text #
+
+        text_surface = self.font.render(self.text, True, self.text_colour)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
+    
+    def click(self):
+        global level
+        if self.text == 'PLAY':
+            level += 1
+        if self.text == 'INSTRUCTIONS':
+            level -= 1
+        if self.text == 'MENU' or 'BACK':
+            level = 0
+
+
+
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -262,35 +302,35 @@ class World():
     #end func
 
 level3_map= [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-[1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1],
-[1,0,0,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-[1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,1],
-[1,0,0,1,1,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,1],
+[1,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1,1,0,0,0,1,0,0,0,0,1],
+[1,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1],
+[1,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,1],
+[1,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,1],
 [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-[1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,1],
-[1,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,6,0,0,1],
-[1,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
-[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,4,4,4,4,1],
+[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,6,0,0,1],
+[1,0,0,1,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1,0,0,0,0,1],
+[1,0,0,0,1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,4,4,4,4,1],
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
 level4_map= [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[1,1,0,0,5,0,5,0,0,0,0,0,5,0,0,5,0,0,0,0,5,0,0,0,1,1],
+[1,1,0,0,0,0,5,0,0,0,0,0,5,0,0,5,0,0,0,0,5,0,0,0,1,1],
 [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
 [1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,6,0,1,1],
 [1,1,4,4,4,4,4,4,4,4,4,4,4,1,0,0,0,0,0,0,0,0,0,0,1,1],
@@ -397,6 +437,7 @@ level1_map= [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
 
+
 # Objects #
 
 player1 = Player(30, 600)
@@ -405,7 +446,7 @@ enemy_group = pygame.sprite.Group()
 
 checkpoint_group = pygame.sprite.Group()
 
-current_level= World(level3_map)
+current_level= World(level1_map)
 
 def reset_level():
     player1.rect.x = 30
@@ -424,10 +465,10 @@ def getlevel():
         current_level= World(level3_map)
     elif level == 4 :
         current_level= World(level4_map)
+    elif level == 5 :
+        current_level= World(level5_map)
+
     return(current_level)
-
-
-
 
 
 # Main Program Loop #
@@ -440,20 +481,116 @@ while not done:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
-    
+        
     # Game logic #
-    if player1.levelDone == True :
-        level += 1
-        reset_level()
-        current_level = getlevel()
-        player1.levelDone = False
+    if level < maxlevel +1 and level > 0 :
+
+        if player1.levelDone :
+            level += 1
+            reset_level()
+            if level < maxlevel +1 :
+                current_level = getlevel()
+            #end if
+            player1.levelDone = False
+
+
+        #end if
+        clock.tick(60)
+        screen.blit(background, (0,0))
+        current_level.draw()
+        enemy_group.update()
+        player1.update()
+        checkpoint_group.update()
+
+    if level == 0:
+        screen.blit(background, (0,0))
+
+        # create objects of the text and button(s) #
+
+        text = font2.render("TINY ROBOT",True,WHITE)
+        button_play = Button(225,225,200,200,'PLAY',font,WHITE,BLACK)
+        button_instructions = Button(100,500,450,75,'INSTRUCTIONS',font,WHITE,BLACK)
+ 
+        # Put the image of the text on the screen #
+        screen.blit(text, [30, 100])
+        button_play.draw(screen)
+        button_instructions.draw(screen)
+
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done = True # Flag that we are done so we exit this loop
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_play.rect.collidepoint(event.pos):
+                    button_play.click()
+                elif button_instructions.rect.collidepoint(event.pos):
+                    button_instructions.click()
+    
+    if level== -1 :
+        screen.blit(background, (0,0))
+
+        # create objects of the text and button(s) #
+
+        line1 = "Use 'UP', 'LEFT' and 'RIGHT' keys "
+        line2 = "to navigate Tiny Robot to the " 
+        line3 = "checkpoint at the end of each level."
+        line4 = "Be careful... you will encouter " 
+        line5 = "enemies and obstacles along the way " 
+        line6 = "which will cause you to die and " 
+        line7 = "respawn at the start of each level. "
+
+
+        text = font.render("HOW TO PLAY",True,WHITE)
+        text2 = font3.render(line1,True, WHITE)
+        text3 = font3.render(line2,True, WHITE)
+        text4 = font3.render(line3,True, WHITE)
+        text5 = font3.render(line4,True, WHITE)
+        text6 = font3.render(line5,True, WHITE)
+        text7 = font3.render(line6,True, WHITE)
+        text8 = font3.render(line7,True, WHITE)
+        text9 = font.render("GOOD LUCK!",True,WHITE)
+        button_back = Button(225,550,200,75,'BACK',font,WHITE,BLACK)
+ 
+        # Put the image of the text on the screen #
+        screen.blit(text, [120, 80])
+        screen.blit(text2, [20, 200])
+        screen.blit(text3, [20, 230])
+        screen.blit(text4, [20, 260])
+        screen.blit(text5, [20, 300])
+        screen.blit(text6, [20, 330])
+        screen.blit(text7, [20, 360])
+        screen.blit(text8, [20, 390])
+        screen.blit(text9, [150, 500])
+
+        button_back.draw(screen)
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done = True # Flag that we are done so we exit this loop
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_back.rect.collidepoint(event.pos):
+                    button_back.click()
+
+    if level> maxlevel :
+        screen.blit(background, (0,0))
+
+        # create objects of the text and button(s) #
+
+        text = font.render("CONGRATULAIONS",True,WHITE)
+        text2 = font.render("YOU WIN!",True,WHITE)
+        button_end = Button(225,450,200,75,'MENU',font,WHITE,BLACK)
+ 
+        # Put the image of the text on the screen #
+        screen.blit(text, [65, 50])
+        screen.blit(text2, [175, 350])
+        button_end.draw(screen)
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done = True # Flag that we are done so we exit this loop
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_end.rect.collidepoint(event.pos):
+                    button_end.click()
     #end if
-    clock.tick(60)
-    screen.blit(background, (0,0))
-    current_level.draw()
-    enemy_group.update()
-    player1.update()
-    checkpoint_group.update()
+
+
 
     pygame.display.update()
     
--- test.py
+++ test.py
@@ -1,216 +1,56 @@
 import pygame
-import random
-import math
-# Initialize the game engine
-pygame.init()
 
-# Define some colors
-WHITE = ((255,255,255))
-BLUE = ((0,0,255))
-GREEN = ((0,255,0))
-RED = ((255,0,0))
-BLACK = ((0,0,0))
-ORANGE = ((255,100,10))
-YELLOW = ((255,255,0))
-BLUE_GREEN = ((0,255,170))
-MARROON = ((115,0,0))
-LIME = ((180,255,100))
-PINK = ((255,100,180))
-PURPLE = ((240,0,255))
-GREY = ((127,127,127))
-MAGENTA = ((255,0,230))
-BROWN = ((100,40,0))
-FOREST_GREEN = ((0,50,0))
-NAVY_BLUE= ((0,0,100))
-RUST = ((210,150,75))
-DANDILION_YELLOW = ((255,200,0))
-HIGHLIGHTER = ((255,255,100))
-SKY_BLUE = ((0,255,255))
-MOONGLOW = ((235,245,255))
-
-# - - # GLOBAL VARIABLES # - - #
-
-gravity = 1
-screen_x =600
-screen_y = 500
-size = (screen_x, screen_y)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("The Tower")
-map =[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
-    [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
-
-clock = pygame.time.Clock()
-
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-#classes go here
-
-step = 0
-class star(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.width = 20
-        self.height = 20
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.fill(WHITE), 
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 100
-        self.step = 0
-        self.vel_y = 0
-    def update(self):
-            self.step += 0.05
-            self.rect.x= 200 +  100* math.sin(self.step)
-     
+class Button:
+    def __init__(self, x, y, width, height, text, font, color, hover_color, text_color, action=None):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.font = font
+        self.color = color
+        self.hover_color = hover_color
+        self.text_color = text_color
+        self.action = action
+        self.hovered = False
+    
+    def draw(self, screen):
+        # Change color on hover
+        current_color = self.hover_color if self.hovered else self.color
+        pygame.draw.rect(screen, current_color, self.rect, border_radius=10)
         
-
-class player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.width = 5
-        self.height = 5
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.fill(WHITE), 
-        self.rect = self.image.get_rect()
-        self.rect.x = 100
-        self.rect.y = 100
-        self.jump_height= 12
-        self.vel_y = 0
-        self.move = 0
+        # Render text
+        text_surface = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
     
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            self.hovered = self.rect.collidepoint(event.pos)
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.hovered and self.action:
+                self.action()
 
-    def update(self):
-        keys = pygame.key.get_pressed() # checks for keys getting pressed
-
-        self.vel_y += gravity # causes the player to fall due to gravity if in the air
-
-        self.rect.y += self.vel_y # causes acceleration downwards for the longer the time in the air
-
-        # Check if player is on the ground
-        if self.rect.bottom > screen_y:
-            self.rect.bottom = screen_y
-            self.vel_y = 0
-
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.move = -5
-            self.rect.x += self.move
-        #end if
-            
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.move = 5
-            self.rect.x += self.move
-        # end if  
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            if self.rect.bottom == screen_y:
-                self.vel_y = -self.jump_height
-        #end if
-        else :
-            self.move=0
-        wall_collisions = pygame.sprite.spritecollide(self, wall_list, False)
-        for wall in wall_collisions:
-            if self.move > 0:
-                self.rect.right = wall.rect.left
-            elif self.move < 0:
-                # Otherwise if we are moving left, do the opposite.
-                self.rect.left = wall.rect.right
-            elif self.rect.bottom > wall.rect.top:
-                self.rect.bottom = wall.rect.top
-                self.vel_y =0
-            elif self.vel_y < 0:
-                self.rect.top = wall.rect.bottom
-            elif self.vel_y==0:
-                self.rect.bottom = wall.rect.top
-#end class
-                
-class Block(pygame.sprite.Sprite):
-    #Define the constructor for block
-    def __init__(self,colour,width,height,x_ref,y_ref):
-        #Call the sprite constructor
-        super().__init__()
-        self.width = width
-        self.height = height
-        #Create a sprite and fill it with colour
-        self.image=pygame.Surface([self.width,self.height])
-        self.image.fill(colour)
-        self.rect=self.image.get_rect()#Set the position of the player attributes
-        self.rect.x=x_ref
-        self.rect.y=y_ref
-
-        def update(self):
-            self=self
-            
-
-#create sprite groups
-all_sprites = pygame.sprite.Group()
-wall_list = pygame.sprite.Group()
-for y in range(25):
-    for x in range(30):
-        if map[y][x]==1:
-            wall=Block(BLUE,20,20,(x*20),(y*20))
-            wall_list.add(wall)
-            all_sprites.add(wall)
-plur=player()
-test = star()
-all_sprites.add(plur)
-all_sprites.add(test)
-
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
+# Example usage
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((500, 400))
+    pygame.display.set_caption("Button Example")
     
-    # --- Game logic should go here
-
-    #check for collisions
+    font = pygame.font.Font(None, 36)
+    def on_click():
+        print("Button Clicked!")
     
-    #update game objects
-    all_sprites.update()
-    test.update()
-    # --- Drawing code should go here
- 
-    # First, clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
+    button = Button(150, 150, 200, 60, "Click Me", font, (0, 128, 255), (0, 200, 255), (255, 255, 255), on_click)
     
+    running = True
+    while running:
+        screen.fill((30, 30, 30))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            button.handle_event(event)
+        
+        button.draw(screen)
+        pygame.display.flip()
     
-    screen.fill(BLACK)
-    
-    #draw stuff here:
-    all_sprites.draw(screen)
-
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-#endwhile
-pygame.quit()
+    pygame.quit()
